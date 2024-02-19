561429724ed38564db3ff8b1c1efdfd07d65724d
Kalu-henry <128081170+Kalu-henry@users.noreply.github.com>
2024-02-19 17:19:18 +0100

    Add files via upload

---
+++ src/sections/index.js
@@ -0,0 +1,13 @@
+import Hero from "./Hero";
+import Footer from "./Footer";
+import CustomerReviews from "./CustomerReviews";
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+import PopularProducts from "./PopularProducts";
+
+
+export {
+    Hero, Footer, CustomerReviews, Services, SpecialOffer, Subscribe, SuperQuality, PopularProducts
+}
